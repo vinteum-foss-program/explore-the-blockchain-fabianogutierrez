f54d4049e2a03e335752c9c7c91c994d18c74e5e
Fabiano Gutierrez <fabianogutierrez@gmail.com>
2024-11-20 02:24:42 -0400

    correcao na saida do desafio 4

--- submission/004.sh
+++ submission/004.sh
@@ -10,15 +10,19 @@ JQ_CMD=jq
 #descriptor=$($BTC_HOME/$BTC_CMD getdescriptorinfo "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/100)")
 #comando=$(echo $descriptor | $JQ_HOME/$JQ_CMD  .descriptor)
 #comando="${comando//\"/$''}"
-#comando="$BTC_HOME/$BTC_CMD deriveaddresses $comando"
-#echo $($comando)
+#resultado=$($BTC_HOME/$BTC_CMD deriveaddresses $comando)
+#resultado="${resultado//\"/$''}"
+#resultado="${resultado//[/$''}"
+#resultado="${resultado//]/$''}"
 
 
 # Teste Github
 descriptor=$($BTC_CMD getdescriptorinfo "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/100)")
 comando=$(echo $descriptor | $JQ_CMD  .descriptor)
-
 comando="${comando//\"/$''}"
+resultado=$($BTC_CMD deriveaddresses $comando)
+resultado="${resultado//\"/$''}"
+resultado="${resultado//[/$''}"
+resultado="${resultado//]/$''}"
 
-comando="$BTC_CMD deriveaddresses $comando"
-echo $($comando)
+echo $resultado
