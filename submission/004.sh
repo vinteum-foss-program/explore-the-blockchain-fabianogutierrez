e582cb4d228d73c585768f2c8d5b54f385c5ba46
Fabiano Gutierrez <fabianogutierrez@gmail.com>
2024-11-20 02:16:06 -0400

    finalizando o desafio 4

--- submission/004.sh
+++ submission/004.sh
@@ -1,2 +1,24 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
+#!/bin/bash
+BTC_HOME=/home/gutierrez/bitcoin/bitcoin/src
+JQ_HOME=/home/gutierrez/bitcoin/tools/jq-1.7.1
+BTC_CMD=bitcoin-cli
+JQ_CMD=jq
+
+# Teste Local
+#descriptor=$($BTC_HOME/$BTC_CMD getdescriptorinfo "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/100)")
+#comando=$(echo $descriptor | $JQ_HOME/$JQ_CMD  .descriptor)
+#comando="${comando//\"/$''}"
+#comando="$BTC_HOME/$BTC_CMD deriveaddresses $comando"
+#echo $($comando)
+
+
+# Teste Github
+descriptor=$($BTC_CMD getdescriptorinfo "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/100)")
+comando=$(echo $descriptor | $JQ_CMD  .descriptor)
+
+comando="${comando//\"/$''}"
+
+comando="$BTC_CMD deriveaddresses $comando"
+echo $($comando)
