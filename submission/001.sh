d21bb1211802fa31e2e5a8e186be1a8bf546dc20
Fabiano Gutierrez <fabianogutierrez@gmail.com>
2024-11-19 21:54:42 -0400

    Execução dos scripts 1 e 2

--- submission/001.sh
+++ submission/001.sh
@@ -1,1 +1,7 @@
 # What is the hash of block 654,321?
+#!/bin/bash
+BTC_HOME=/home/gutierrez/bitcoin/bitcoin/src
+JQ_HOME=/home/gutierrez/bitcoin/tools/jq-1.7.1
+
+eval "$BTC_HOME/bitcoin-cli getblockhash 654321"
+
--- submission/002.sh
+++ submission/002.sh
@@ -2,3 +2,15 @@
 #   address: `1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa`
 #   message: `1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa`
 #   signature: `HCsBcgB+Wcm8kOGMH8IpNeg0H4gjCrlqwDf/GlSXphZGBYxm0QkKEPhh9DTJRp2IDNUhVr0FhP9qCqo2W0recNM=`
+#!/bin/bash
+BTC_HOME=/home/gutierrez/bitcoin/bitcoin/src
+JQ_HOME=/home/gutierrez/bitcoin/tools/jq-1.7.1
+
+address="1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa"
+signature="HCsBcgB+Wcm8kOGMH8IpNeg0H4gjCrlqwDf/GlSXphZGBYxm0QkKEPhh9DTJRp2IDNUhVr0FhP9qCqo2W0recNM="
+message="1E9YwDtYf9R29ekNAfbV7MvB4LNv7v3fGa"
+resultado=$($BTC_HOME/bitcoin-cli verifymessage $address $signature $message)
+echo $resultado
+
+echo $BTC_HOME/bitcoin-cli verifymessage $address $signature $message
+
