c0d33cb6dec46f349de0a0df6d3c02ef04ebc741
Fabiano Gutierrez <fabianogutierrez@gmail.com>
2024-11-24 20:56:31 -0400

    ultimo desafio- corrigido

--- submission/008.sh
+++ submission/008.sh
@@ -8,12 +8,25 @@ BTC_CMD=bitcoin-cli
 JQ_CMD=jq
 
 # Teste Local
-hash=$($BTC_HOME/$BTC_CMD getrawtransaction e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163)
-bloco=$($BTC_HOME/$BTC_CMD decoderawtransaction $hash )
-tx=$(echo $bloco | $JQ_HOME/$JQ_CMD -r '.vin[0].txinwitness[]')
+#hash=$($BTC_HOME/$BTC_CMD getrawtransaction e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163)
+#bloco=$($BTC_HOME/$BTC_CMD decoderawtransaction $hash )
+#tx=$(echo $bloco | $JQ_HOME/$JQ_CMD -r '.vin[0].txinwitness[]')
+#for hexa in $tx; do
+#    decode=$($BTC_HOME/$BTC_CMD decodescript $hexa)
+#    asm=$(echo $decode | $JQ_HOME/$JQ_CMD -r '.asm |split(" ") |.[1]')
+#    qnt=${#asm}
+#    if [ "$qnt" -eq 66 ]; then
+#        echo $asm 
+#    fi
+#done
+
+# Teste git
+hash=$($BTC_CMD getrawtransaction e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163)
+bloco=$($BTC_CMD decoderawtransaction $hash )
+tx=$(echo $bloco | $JQ_CMD -r '.vin[0].txinwitness[]')
 for hexa in $tx; do
-    decode=$($BTC_HOME/$BTC_CMD decodescript $hexa)
-    asm=$(echo $decode | $JQ_HOME/$JQ_CMD -r '.asm |split(" ") |.[1]')
+    decode=$($BTC_CMD decodescript $hexa)
+    asm=$(echo $decode | $JQ_CMD -r '.asm |split(" ") |.[1]')
     qnt=${#asm}
     if [ "$qnt" -eq 66 ]; then
         echo $asm 
